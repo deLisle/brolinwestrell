7f071855d5e54fb9af691f961ec53759bfcc41cb
vagrant <vagrant@dev.localhost>
2015-12-01 15:22:41 +0000

    add missing files

---
+++ lib/homepage-featured-posts-code-box.php
@@ -0,0 +1,70 @@
+<?php
+namespace Roots\Sage\HomepageFeaturedPostCodeBox;
+use Roots\Sage\TemplateMetaBox;
+use Roots\Sage\CodeBox;
+
+/**
+* Implement CodeBox class for the post/page that uses front-page.php template file.
+*/
+class HomepageFeaturedPostCodeBox extends CodeBox\CodeBox
+{
+
+  public function __construct()
+  {
+    add_action( 'add_meta_boxes_page',  array( $this, 'register' ) );
+    add_action( 'save_post',  array( $this, 'save' ) );
+  }
+  
+  /**
+   * Inintite TemplateMetaBox class and setup the metaboxes to be shown in the admin page
+   **/
+  public function register()
+  {
+    global $post;
+  
+    $setup = new TemplateMetaBox\TemplateMetaBox($post);
+
+    // top section
+    $setup->add_meta_box(
+      'homepage_featured_posts',
+      'Homepage Second Content',
+      function($post) {
+        // Add a nonce field so we can check for it later.
+        wp_nonce_field( 'homepage_featured_posts[]', 'brolinwestrell_new_nonce' );
+
+        $values = get_post_meta( $post->ID, '_homepage_featured_posts', true );
+        $posts = \query_posts('posts_per_page=-1&post_type=post&status=published');
+        echo "<select name=\"homepage_featured_posts[]\" multiple >";
+        foreach ($posts as $post) {
+            if( in_array($post->ID, $values) ) {
+                echo "<option selected value=\"". $post->ID ."\">{$post->post_title}</option>";
+            } else {
+                echo "<option value=\"". $post->ID ."\">{$post->post_title}</option>";
+            }
+        }
+        echo "</select>";
+      },
+      'page', 'advanced', 'core'
+    );
+
+    $setup->init(null, 'home');
+  }
+
+  /** 
+   * Save post handler
+   */
+  function save($post_id) {
+    
+    parent::save($post_id);
+    
+    if (  !isset( $_POST['homepage_featured_posts'] ) ) {
+      return;
+    }
+    // add more if needed
+      // var_dump($_POST['homepage_featured_posts'] ); exit;
+    update_post_meta( $post_id, '_homepage_featured_posts', $_POST['homepage_featured_posts'] );
+  }
+
+}
+//Initiate the class
+HomepageFeaturedPostCodeBox::init();
