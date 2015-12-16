4335eeedd5caa02a9a018e3109e5f461de05404c
Andrew de Lisle <andrew@Andrews-MacBook-Pro-2.local>
2015-12-16 23:23:28 +0100

    more fixes

--- templates/content.php
+++ templates/content.php
@@ -1,5 +1,7 @@
+<?php if ( $_SERVER["REQUEST_URI"] == '/nyheter/' ) { ?>
 <p>Under de snart tio år som BrolinWestrell funnits har det hänt en del. I nyhetsarkivet har vi samlat en del av det vi velat berätta om under åren och en del som media uppmärksammat.</p>
 <br />
+<?php }?>
 <article <?php post_class('inner-container'); ?>>
   <header>
     <?php get_template_part('templates/entry-meta'); ?>
