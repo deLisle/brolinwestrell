2015fba81e30f1887a3a5cda7a0516cd5fefa4a2
vagrant <vagrant@dev.localhost>
2016-01-31 04:10:43 +0000

    blue background in about us page

--- page-about.php
+++ page-about.php
@@ -10,31 +10,4 @@
 <?php endwhile; ?>
 <?php wp_reset_query(); ?>
 <?php $portfolios = \query_posts('posts_per_page=-1&post_type=employee_portfolios');
-?><div id="employees"><?php
-while (have_posts()) : the_post();
-	$metas = get_post_meta ( get_the_ID(), null, true );
-	?>
-	<div class="employee">
-		<?php
-		if ( has_post_thumbnail() ) {
-			$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
-			// var_dump($large_image_url);
-			echo '<img width="'.$large_image_url[1].'" height="'.$large_image_url[2].'" class="attachment-large wp-post-image large" src="'.$large_image_url[0].'" />';
-		}
-		?>
-		<h3><?php the_title(); ?></h3>
-		<?php foreach ($metas as $key => $value) {
-			$key = trim($key);
-	    	if ( '_' == $key{0} )
-	        	continue;
-			echo '<div class="'.strtolower($key).'">'.$value[0].'</div>';
-		}
-		?>
-		<div class="employee-description">
-		<?php the_content();?>
-		</div>
-	</div>
-	<?php
-endwhile;
 ?>
-</div>
--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,35 @@
-<?php if ( is_front_page() ): ?>
+<?php if ( is_page_template( 'page-about.php' ) ): ?>
+<div class="container-fluid secondary-content">
+		<div class="container content row text-center">
+			<div id="employees"><?php
+			while (have_posts()) : the_post();
+				$metas = get_post_meta ( get_the_ID(), null, true );
+				?>
+				<div class="employee">
+					<?php
+					if ( has_post_thumbnail() ) {
+						$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
+						// var_dump($large_image_url);
+						echo '<img width="'.$large_image_url[1].'" height="'.$large_image_url[2].'" class="attachment-large wp-post-image large" src="'.$large_image_url[0].'" />';
+					}
+					?>
+					<h3><?php the_title(); ?></h3>
+					<?php foreach ($metas as $key => $value) {
+						$key = trim($key);
+				    	if ( '_' == $key{0} )
+				        	continue;
+						echo '<div class="'.strtolower($key).'">'.$value[0].'</div>';
+					}
+					?>
+					<div class="employee-description">
+					<?php the_content();?>
+					</div>
+				</div>
+				<?php
+			endwhile;
+			?>
+</div></div></div>
+<?php endif; if ( is_front_page() ): ?>
 	<div class="container-fluid secondary-content">
 		<div class="container content row text-center">
 			<?php
